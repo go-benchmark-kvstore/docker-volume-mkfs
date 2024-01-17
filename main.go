b20065e8aed427ea5379ae2fe0947187e0cf2760
Mitar <mitar.git@tnode.com>
2024-01-17 22:53:57 +0100

    Try with zerolog.SyncWriter.

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	"os"
 	"os/user"
 	"strconv"
 	"sync"
 
 	"github.com/alecthomas/kong"
 	"github.com/docker/go-plugins-helpers/volume"
+	"github.com/rs/zerolog"
 	"gitlab.com/tozd/go/cli"
 	"gitlab.com/tozd/go/errors"
-	"gitlab.com/tozd/go/zerolog"
+	z "gitlab.com/tozd/go/zerolog"
 )
 
 //nolint:lll
 type Driver struct {
-	zerolog.LoggingConfig
+	z.LoggingConfig
 
 	Partitions []string `arg:""                help:"Partition(s) to use."                                            name:"partition"                   required:""           type:"path"`
 	Dir        string   `       default:"/mnt" help:"Directory under which to mount partitions. Default: ${default}."                  placeholder:"DIR"             short:"d" type:"path"`
@@ -31,6 +33,9 @@ type Driver struct {
 
 func main() {
 	var driver Driver
+	// os.Stdout is generally already synchronized, but messages to it
+	// are still being lost. So we wrap it into zerolog.SyncWriter.
+	driver.LoggingConfig.Logging.Console.Output = zerolog.SyncWriter(os.Stdout)
 	cli.Run(&driver, nil, func(ctx *kong.Context) errors.E {
 		driver.volumes = make(map[string]string)
 		driver.mounts = make(map[string][]string)
